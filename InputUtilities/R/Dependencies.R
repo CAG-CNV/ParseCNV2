5d9b8fbf5288cb04d3860738ec977c7a5b477a91
joeglessner1 <31705551+joeglessner1@users.noreply.github.com>
2021-03-13 22:31:44 -0500

    Use tmp for R library install to avoid permission issues

--- InputUtilities/R/Dependencies.R
+++ InputUtilities/R/Dependencies.R
@@ -1,3 +1,5 @@
+Sys.setenv(TMPDIR="./tmp")
+configure.vars="TMPDIR=./tmp"
 list.of.packages <- c("stringr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages,repos="http://cran.at.r-project.org")
+if(length(new.packages)) install.packages(new.packages,repos="http://cran.r-project.org")
